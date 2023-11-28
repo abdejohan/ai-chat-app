c507a6b82d090a231b2062b62dc38bbd298d1050
Johan Abde <abdejohan@gmail.com>
2023-11-28 22:00:35 +0100

    Install eslint unicorn plugin

--- web/src/pages/Typography.tsx
+++ web/src/pages/Typography.tsx
@@ -1,17 +1,15 @@
-const Typography = () => {
-  return (
-    <main className="page typography-test-container">
-      <h1>h1: Main Heading </h1>
-      <h2>h2: Section Heading</h2>
-      <h3>h3: Subsection Heading</h3>
-      <h4>h4: Subsection Heading</h4>
-      <h5>h5: Subsection Heading</h5>
-      <h6>h6: Subsection Heading</h6>
-      <label>label: Your Information Here</label>
-      <p>p: Engaging Content Awaits!</p>
-      <a href="#">a: Explore More</a>
-    </main>
-  );
-};
+const Typography = () => (
+  <main className="page typography-test-container">
+    <h1>h1: Main Heading </h1>
+    <h2>h2: Section Heading</h2>
+    <h3>h3: Subsection Heading</h3>
+    <h4>h4: Subsection Heading</h4>
+    <h5>h5: Subsection Heading</h5>
+    <h6>h6: Subsection Heading</h6>
+    <label>label: Your Information Here</label>
+    <p>p: Engaging Content Awaits!</p>
+    <a href="#">a: Explore More</a>
+  </main>
+);
 
 export default Typography;
